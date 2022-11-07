fe1486de348ea7247706303a6ebcacacfc7394cf
sx19990201 <1291516667@qq.com>
2022-11-07 19:39:42 +0800

    update shell

--- fireboom.sh
+++ fireboom.sh
@@ -4,4 +4,7 @@ cd wundergraph/ && yarn
 cd ..
 curl -o fireboom https://markdown-file-1259215954.cos.ap-nanjing.myqcloud.com/fireboom
 chmod -R 777 fireboom
+curl -o wunderctl https://markdown-file-1259215954.cos.ap-nanjing.myqcloud.com/wunderctl
+chmod -R 777 wunderctl
+cp -r wunderctl /usr/local/bin/
 ./fireboom
